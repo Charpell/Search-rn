dda74f4d68ade4a0298d50725e18712c850f84d8
charpell <charpellumeh@gmail.com>
2019-11-11 13:27:10 +0100

    finish

--- App.js
+++ App.js
@@ -1,19 +1,39 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Platform } from 'react-native';
+import Landing from './src/Landing';
+import Search from './src/Search';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+export default class App extends React.Component {
+  state = {
+    currentScreen: "landing"
+  }
+  switchScreen = (screen) => {
+    this.setState({currentScreen: screen})
+  }
+  renderScreen = () =>{
+    if(this.state.currentScreen === "landing"){
+      return(
+        <Landing switchScreen={this.switchScreen}/>
+      )
+    }
+    else if(this.state.currentScreen === "search"){
+      return(
+        <Search />
+      )
+    }
+  }
+  render() {
+    return (
+      <View style={styles.container}>
+        {this.renderScreen()}
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    marginTop: Platform.OS === 'android' ? 24 : 0
   },
 });
